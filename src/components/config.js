698e8c7ea089860bb676e76f3afbc0af63914c37
Mohit Prajapati <119717966+mkraj-7838@users.noreply.github.com>
2025-07-04 21:26:21 +0530

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
 // export const BASE_API_URL = 'http://localhost:5000/api';
 
-export const BASE_API_URL = 'https://restaurant-review-manage.onrender.com';
+export const BASE_API_URL = 'https://restaurant-review-manage.onrender.com/api';
 
